72935be68c2fe60102cb5e0be097df148a3dc43e
Victor Pirottin <vpirotti@z4r3p1.42lyon.fr>
2026-06-03 15:02:46 +0200

    first commit

---
+++ ex00/inc/templates.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*                                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpirotti <vpirotti@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   GitHub : @Nave20                                  #+#    #+#             */
+/*   28 is the new 42                                 ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+template <typename T>
+T max(T a, T b) { return a > b ? a : b; }
+
+template <typename T>
+T min(T a, T b) { return a < b ? a : b; }
+
+template <typename T>
+void swap(T& a, T& b) { T tmp = a; a = b; b = tmp; }
---
+++ ex00/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*                                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vpirotti <vpirotti@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   GitHub : @Nave20                                  #+#    #+#             */
+/*   28 is the new 42                                 ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "../inc/templates.hpp"
+
+int main()
+{
+	int a = 28;
+	int b = 42;
+
+	std::cout << "a : " << a << std::endl;
+	std::cout << "b : " << b << std::endl;
+
+	std::cout << "SWAP : " << std::endl;
+	swap(a, b);
+	std::cout << "a : " << a << std::endl;
+	std::cout << "b : " << b << std::endl;
+	std::cout << "MAX : " << max(a, b) << std::endl;
+	std::cout << "MIN : " << min(a, b) << std::endl;
+}
