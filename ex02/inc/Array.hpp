264c3c68bc3047e09fc86bfde3cab911c066cce8
Nave20 <pirottinv@gmail.com>
2026-07-28 15:21:12 +0200

    Array

--- ex02/inc/Array.hpp
+++ ex02/inc/Array.hpp
@@ -19,39 +19,44 @@ class Array {
 	public:
 		Array(): _array(NULL), _size(0) {};
 		Array(unsigned int n): _array(new T[n]), _size(n) {};
-		Array(const Array& copy): _array(NULL), _size(copy._size) {
+		Array(const Array& copy): _array(NULL), _size(copy._size)
+		{
 			_array = new T[_size];
-			for (size_t i = 0; i < _size; i++) {
+			for (size_t i = 0; i < _size; i++)
+				{
 				_array[i] = copy._array[i];
 			}
 		};
-		Array& operator=(const Array& other) {
-			if (this != &other) {
+		Array& operator=(const Array& other)
+		{
+			if (this != &other)
+				{
 				delete[] _array;
 				_size = other._size;
 				_array = new T[_size];
-				for (size_t i = 0; i < _size; i++) {
+				for (size_t i = 0; i < _size; i++)
+				{
 					_array[i] = other._array[i];
 				}
 			}
 			return (*this);
 		};
-		~Array() {
-			delete[] _array;
-		};
-		unsigned int size() const {
-			return (_size);
-		}
-		T& operator[](unsigned int index) {
+		~Array() {delete[] _array;};
+
+		unsigned int size() const {return (_size);}
+		T& operator[](unsigned int index)
+		{
 			if (index >= _size)
 				throw std::out_of_range("Out of bounds");
 			return (_array[index]);
 		};
-		const T& operator[](unsigned int index) const {
+		const T& operator[](unsigned int index) const
+		{
 			if (index >= _size)
 				throw std::out_of_range("Out of bounds");
 			return (_array[index]);
 		};
+
 	private:
 		unsigned int	_size;
 		T*				_array;
